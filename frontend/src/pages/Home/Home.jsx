c706e83cec58bddc78a60f3993bab21c158f4aa2
Shaurya0786 <shauryadhawn0786@gmail.com>
2024-07-24 18:17:26 +0530

    Menu sections Setup

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 function Home(){
   return (
     <div>
         <Header/>
+        <ExploreMenu/>
       Home Page
     </div>
   )
